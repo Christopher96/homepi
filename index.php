7d396e3e40f5635f8999845b87b4f066ab6679d4
Christopher Gauffin <christopher.gauffin@gmail.com>
2018-03-01 17:53:14 +0100

    asdf

--- index.php
+++ index.php
@@ -2,13 +2,23 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>HomePi</title>
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
     <div class="container">
-        <div id="compCircle" class="circle"></div>
-        <button id="compButton" class="btn">Wake Computer</button>
+        <div class="circle-box">
+            <div id="circle" class="circle"></div>
+        </div>
+        <span id="status">Computer is offline</span></br>
+        <span id="os_status"></span>
+        <button id="wakeBtn" class="btn">Wake Computer</button>
+        <div class="operateBtns">
+            <button id="restartBtn" class="btn" disabled>Restart OS</button>
+            <button id="shutdownBtn" class="btn" disabled>Shutdown OS</button>
+            <button id="switchBtn" class="btn" disabled>Switch OS</button>
+        </div>
     </div>
 </body>
 <script src="js/script.js"></script>
--- js/script.js
+++ js/script.js
@@ -9,8 +9,12 @@ function apiCall(action, callback){
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function(){
         if(this.readyState == 4 && this.status == 200){
-            var json = JSON.parse(this.response);
-            callback(json);
+            try {
+                var json = JSON.parse(this.response);
+                callback(json);
+            } catch(e) {
+                console.log(this.response);
+            }
         }
     }
     xhr.open("POST", "php/api.php");
@@ -20,39 +24,113 @@ function apiCall(action, callback){
 }
 
 var timeout = 30;
-var wolSent = false;
 
-function checkAwake() {
-    apiCall("checkAwake", function(res){
+var wakeBtn = $("#wakeBtn");
+var stsTxt = $("#status");
+
+var pingTimer;
+
+function startPinging() {
+    clearInterval(pingTimer);
+    pingTimer = setInterval(ping, 1000);
+}
+
+function stopPinging() {
+    clearInterval(pingTimer);
+}
+
+function sts(message, color){
+    stsTxt.innerHTML = message;
+    $("#circle").style.backgroundColor = color;
+}
+
+function ping() {
+    apiCall("ping", function(res){
         if(res.success) {
-            $("#compCircle").style.backgroundColor = (res.body) ? "green" : "red";
-            if(res.body) {
-                $("#compButton").innerHTML = "Computer is awake";
-                $("#compButton").setAttribute("disabled", true);
-            } else {
-                $("#compButton").innerHTML = (wolSent) ? "Computer is waking..." : "Wake computer";
-                if(wolSent) {
-                    $("#compButton").setAttribute("disabled", true);
-                } else {
-                    $("#compButton").removeAttribute("disabled");
-                }
-            }
+            stopPinging();
+            wakeBtn.setAttribute("disabled", true);
+            sts(res.body, "green");
+            getOS();
+        } else {
+            sts(res.body, "red");
         }
-        setTimeout(checkAwake, 1000);
     });
 }
 
-$("#compButton").addEventListener("click", function() {
-    wolSent = true;
+function getOS() {
+    sts("Fetching current OS...", "yellow");
+    apiCall("getOS", function(res) {
+        if(res.success) {
+            sts("Waiting for operations", "green");
+            $("#os_status").innerHTML = "Current OS: " + res.body;
+            var btns = $(".operateBtns .btn");
+            for(i=0; i<btns.length; i++) {
+                btns[i].removeAttribute("disabled");
+            }
+        } else {
+            startPinging();
+            $("#os_status").innerHTML = "";
+            sts(res.body, "red");
+        }
+    });
+}
+
+$("#restartBtn").addEventListener("click", function() {
+    sts("Restarting computer...", "yellow");
+    apiCall("restart", function(res) {
+        if(res.success) {
+            setTimeout(startPinging, 5000);
+        } else {
+            sts(res.body, "red");
+            startPinging();
+        }
+    })
+});
+$("#shutdownBtn").addEventListener("click", function() {
+    sts("Shutting down computer...", "yellow");
+    apiCall("shutdown", function(res) {
+        if(res.success) {
+            setTimeout(startPinging, 5000);
+        } else {
+            sts(res.body, "red");
+            startPinging();
+        }
+    })
+});
+
+$("#switchBtn").addEventListener("click", function() {
+    sts("Switching OS...", "yellow");
+    apiCall("switch", function(res) {
+        if(res.success) {
+            setTimeout(startPinging, 5000);
+        } else {
+            sts(res.body, "red");
+            startPinging();
+        }
+    })
+});
+
+wakeBtn.addEventListener("click", function() {
+    wakeBtn.setAttribute("disabled", "true");
+    sts("Computer is waking...", "yellow");
     apiCall("wol", function() {
         if(res.success) {
+            sts(res.body, "green");
             setTimeout(function()  {
-                wolSent = false;
+                if(!isAwake) {
+                    sts("WOL did not wake computer", "red");
+                    wakeBtn.innerHTML = "Wake Computer";
+                }
             }, timeout);
         } else {
-            wolSent = false;
+            startPinging();
+            sts(res.body, "red");
+            wakeBtn.innerHTML = "Wake Computer";
         }
+        $("#compButton").removeAttribute("disabled");
     });
 });
 
-checkAwake();
+startPinging();
+ping();
+
